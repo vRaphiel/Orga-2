e55bf98765dbec8885c5985ce902e99e9b418456
Victor Asmad <vraphieljr@gmail.com>
2023-05-15 08:54:43 -0300

    FEAT - Parciales

---
+++ Practica/P-V2/entregable/parcial-bundle.v0.1/solucion/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <assert.h>
+
+// Nombre: ASMAD MURGA VICTOR MANUEL
+// LU: 760/19
+#include "ejs.h"
+
+int main (void){
+	/* Acá pueden realizar sus propias pruebas */
+	return 0;    
+}
